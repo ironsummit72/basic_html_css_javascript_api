d27ca596c30bbfaf6711a23f29e1feb9da7d3fc0
ironsummit72 <ironsumit72@gmail.com>
2023-09-15 14:24:16 +0530

    complete

--- basic_frontend/script.js
+++ basic_frontend/script.js
@@ -1,26 +1,85 @@
-
-let data=fetchData()
-async function fetchData()
-{
-let response=await fetch(`http://localhost:3000/drivers`);
-let data=await response.json()
-return data;
+let data = fetchData();
+async function fetchData() {
+  let response = await fetch(`http://localhost:3000/drivers`);
+  let data = await response.json();
+  return data;
 }
-data.then((value)=>{
-    value.forEach(element => {
-        h1(element.driver)
+
+// * user input
+const div = document.createElement("div");
+const input = document.createElement("input");
+const buttonSubmit = document.createElement("button");
+input.className = "userInput";
+buttonSubmit.className = "submit";
+div.className = "container";
+input.setAttribute("type", "text");
+input.setAttribute("placeholder", "Enter the name of the driver to add");
+buttonSubmit.innerText = "Add";
+div.appendChild(input);
+div.appendChild(buttonSubmit);
+document.body.appendChild(div);
+buttonSubmit.addEventListener("click", submit);
+async function submit() {
+  let driver = input.value.toString();
+
+  let data = await fetch(`http://localhost:3000/drivers/${driver}`, {
+    method: "POST",
+  });
+  let response = data.json();
+  response
+    .then((response) => {
+      if (response) {
+        input.value = "";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
     });
-})
+}
 
+const message = document.createElement("h1"); // ? message if no items found
+// * create list
+data.then((value) => {
+  if (value.length === 0) {
+    message.innerText = "No items";
+    message.className = "message";
+    document.body.appendChild(message);
+  } else {
+    message.style.display = "none";
+  }
 
+  value.forEach((element) => {
+    h1(element.driver);
+  });
+});
 
-function h1(text)
-{
-    const h1=document.createElement('h1');
-    h1.innerHTML=text;
-    h1.style.backgroundColor="orange"
-    h1.style.color="white"
-    h1.style.fontFamily='sans-serif'
-    h1.style.padding='10px'
-    document.body.appendChild(h1);   
+function h1(text) {
+  const deleteBtn = document.createElement("button");
+
+  const h1 = document.createElement("h1");
+  const div = document.createElement("div");
+
+  deleteBtn.innerText = "Delete";
+  deleteBtn.style.display = "inline-block";
+  deleteBtn.className = "delete";
+  div.className = "list-item";
+  h1.innerHTML = text;
+  div.style.fontFamily = "sans-serif";
+
+  div.style.margin = "10px";
+  div.appendChild(h1);
+  div.appendChild(deleteBtn);
+  document.body.appendChild(div);
+
+  deleteBtn.addEventListener("click", async () => {
+    console.log(h1.innerText);
+    const driverName = h1.innerText.toString();
+    let data = await fetch(`http://localhost:3000/drivers/${driverName}`, {
+      method: "DELETE",
+    });
+    let response = data.json();
+    response.then((data) => {
+      div.style.display = "none";
+    });
+  });
 }
