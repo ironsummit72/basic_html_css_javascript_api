97c6f23b93cda6e6e6103429cba210e23c740d87
ironsummit72 <ironsumit72@gmail.com>
2023-09-15 14:25:58 +0530

    added delete api and also add cors

--- basic_backend/app.js
+++ basic_backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+const cors=require('cors');
 const port = 3000;
 const connection = mysql.createConnection({
   user: "root",
@@ -8,21 +9,23 @@ const connection = mysql.createConnection({
   password: "root12345",
   database: "formula",
 });
-connection.connect((err) => {
-  if (err) throw err;
+app.use(cors());
+
+connection.connect((error) => {
+  if (error) throw error;
   console.log("connection success");
 });
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+app.use((request,response,next)=>{
+  response.setHeader('Access-Control-Allow-Origin','*');
+  response.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  response.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
   next(); 
 })
 
-app.get("/drivers", (req, response) => {
-  connection.query(`SELECT * from drivers`, (err, result) => {
-    if (err) {
-      throw err;
+app.get("/drivers", (request, response) => {
+  connection.query(`SELECT * from drivers`, (error, result) => {
+    if (error) {
+      throw error;
     }
     response.json(result);
   });
@@ -31,10 +34,10 @@ app.post("/drivers/:driver", (request, response) => {
   let driver = request.params.driver;
   connection.query(
     `insert into drivers values ('${driver}')`,
-    (err, result) => {
-      if (err) {
+    (error, result) => {
+      if (error) {
         response.json({
-          message: `error occured ${err}`,
+          message: `error occured ${error}`,
         });
       }
       response.json(result);
@@ -42,9 +45,23 @@ app.post("/drivers/:driver", (request, response) => {
   );
 });
 
-app.get("", (req, res) => {
-  res.send("hello user");
+app.get("", (request, response) => {
+  response.send("hello user");
 });
+app.delete('/drivers/:driver',(request,response)=>{
+  let driver=request.params.driver;
+  connection.query(`DELETE FROM drivers where driver='${driver}'`,(error,result)=>{
+     if(error)
+     {
+      response.json({
+        "message":`Something went wrong ${error}`
+      })
+     }
+     response.json({result})
+     
+
+  })
+})
 
 app.listen(port, () => {
   console.log(`listining on localhost:${port}`);
